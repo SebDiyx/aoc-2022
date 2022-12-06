e2446dbfa65f1428c8e7bb857d881f8a1c98c83b
sebastiandixon <cyclewithsebastian@gmail.com>
2022-12-06 14:30:14 +0800

    Day 6

---
+++ 6/a.ts
@@ -0,0 +1,20 @@
+export {};
+
+const input = await Deno.readTextFile('./input.txt');
+
+// Searching for 4 non repeating chars
+
+for (let ii = 4; ii <= input.length; ii++) {
+    const group = input.slice(ii - 4, ii);
+    let isSignal = true;
+    for (const char of group) {
+        if (group.lastIndexOf(char) !== group.indexOf(char)) {
+            isSignal = false;
+            break;
+        }
+    }
+    if (isSignal) {
+        console.log(ii);
+        break;
+    }
+}
